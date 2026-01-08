ba3152a7877ee07ec60d9774c1870270fc6a5d54
koinyan nelson <koinyannelson0@gmail.com>
2026-01-08 13:46:58 +0100

    feat: add delete book functionality

--- src/components/BookTable.tsx
+++ src/components/BookTable.tsx
@@ -1,31 +1,49 @@
-import { useQuery } from "@apollo/client/react";
-import { Table, Spinner } from "@chakra-ui/react";
-import { GET_BOOKS } from "../graphql/books";
+import { useMutation, useQuery } from "@apollo/client/react";
+import { Table, Spinner, Button } from "@chakra-ui/react";
+import { DELETE_BOOK, GET_BOOKS } from "../graphql/books";
+import CreateBookModal from "./CreateBookModal";
 
 export default function BookTable() {
-  const { data, loading } = useQuery<GetBooksResponse>(GET_BOOKS);
+    const [deleteBook, { loading: deleting }] = useMutation(DELETE_BOOK, {
+        refetchQueries: [GET_BOOKS],
+    });
+    const { data, loading: fetching } = useQuery<GetBooksResponse>(GET_BOOKS);
 
-  if (loading) return <Spinner />;
+    if (fetching) return <Spinner />;
 
-  return (
-    <Table.Root mt={6}>
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeader>Name</Table.ColumnHeader>
-          <Table.ColumnHeader>Description</Table.ColumnHeader>
-        </Table.Row>
-      </Table.Header>
+    return (
+        <>
+            <CreateBookModal />
 
-      <Table.Body>
-        {data?.books.map(
-          (book: { id: string; name: string; description: string }) => (
-            <Table.Row key={book.id}>
-              <Table.Cell>{book.name}</Table.Cell>
-              <Table.Cell>{book.description}</Table.Cell>
-            </Table.Row>
-          )
-        )}
-      </Table.Body>
-    </Table.Root>
-  );
+            <Table.Root mt={6}>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.ColumnHeader>Name</Table.ColumnHeader>
+                        <Table.ColumnHeader>Description</Table.ColumnHeader>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {data?.books.map(
+                        (book: { id: string; name: string; description: string }) => (
+                            <Table.Row key={book.id}>
+                                <Table.Cell>{book.name}</Table.Cell>
+                                <Table.Cell>{book.description}</Table.Cell>
+                                <Table.Cell>
+                                    <Button
+                                        disabled={deleting}
+                                        colorScheme="red"
+                                        size="sm"
+                                        onClick={() => deleteBook({ variables: { id: book.id } })}
+                                    >
+                                        Delete {deleting && <Spinner />}
+                                    </Button>
+                                </Table.Cell>
+                            </Table.Row>
+                        )
+                    )}
+                </Table.Body>
+            </Table.Root>
+        </>
+    );
 }
